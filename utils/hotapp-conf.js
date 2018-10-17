82c625116d89ba50e193ed2755deac2022ff045b
1303434374 <35438089+1303434374@users.noreply.github.com>
2018-10-17 17:33:21 +0800

    清空

--- utils/hotapp-conf.js
+++
@@ -1,1 +0,0 @@
-exports.hotAppKey = "hotapp230874041", exports.wxAuth = "1", exports.appVer = "1.1.7";
--- utils/messageutil.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    showtips: function(o, t) {
-        return 0 == o ? "已拼" + o + "件,拼购" + t + "件成团！" : o < t ? "已拼" + o + "件,差" + (t - o) + "件成团！" : o >= t ? "已拼" + o + "件,我也要拼！" : void console.log(o);
-    },
-    showchengtuantiops: function(o, t) {
-        return 0 == o ? "成团后即向档口报单发货，不成团退款！" : o < t ? "快成团了，约熟悉的店主一起拼～" : o >= t ? "已成团，即将向档口报单，想拼的赶紧下单哈！" : void 0;
-    }
-};
